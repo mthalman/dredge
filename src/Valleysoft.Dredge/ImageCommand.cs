cd584af46b4784bac4cc246d73d67c620786d498
Matt Thalman <mthalman@microsoft.com>
2022-10-30 10:00:20 -0500

    Adds image command with inspect support

---
+++ src/Valleysoft.Dredge/ImageCommand.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using System.CommandLine;
+using Valleysoft.DockerRegistryClient;
+using Valleysoft.DockerRegistryClient.Models;
+
+namespace Valleysoft.Dredge;
+
+public class ImageCommand : Command
+{
+    public ImageCommand() : base("image", "Commands related to container images")
+    {
+        AddCommand(new InspectCommand());
+    }
+
+    private class InspectCommand : Command
+    {
+        public InspectCommand() : base("inspect", "Return low-level information on a container image")
+        {
+            Argument<string> imageArg = new("image", "Name of the container image (<image>, <image>:<tag>, or <image>@<digest>)");
+            AddArgument(imageArg);
+            this.SetHandler(ExecuteAsync, imageArg);
+        }
+
+        private Task ExecuteAsync(string image)
+        {
+            ImageName imageName = ImageName.Parse(image);
+            return CommandHelper.ExecuteCommandAsync(imageName.Registry, async () =>
+            {
+                using DockerRegistryClient.DockerRegistryClient client = await CommandHelper.GetRegistryClientAsync(imageName.Registry);
+                ManifestInfo manifestInfo = await client.Manifests.GetAsync(imageName.Repo, (imageName.Tag ?? imageName.Digest)!);
+                
+                if (manifestInfo.MediaType.Contains("list"))
+                {
+                    throw new NotSupportedException(
+                        $"The image name '{image}' has a media type of '{manifestInfo.MediaType}' which does not represent an image. Resolve the underlying image and provide that name instead.");
+                }
+                
+                if (manifestInfo.Manifest is not DockerManifestV2 manifest)
+                {
+                    throw new NotSupportedException(
+                        $"The image name '{image}' has a media type of '{manifestInfo.MediaType}' which is not supported.");
+                }
+
+                string? digest = manifest.Config?.Digest;
+
+                if (digest is null)
+                {
+                    throw new NotSupportedException($"Could not resolve the image config digest of {image}.");
+                }
+
+                Stream blob = await client.Blobs.GetAsync(imageName.Repo, digest);
+                using StreamReader reader = new(blob);
+                string content = await reader.ReadToEndAsync();
+                object json = JsonConvert.DeserializeObject(content);
+                string output = JsonConvert.SerializeObject(json, Formatting.Indented);
+                Console.Out.WriteLine(output);
+            });
+        }
+    }
+}
