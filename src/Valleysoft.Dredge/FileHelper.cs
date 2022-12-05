b39a5fef1a37b8f9fd7cc57e6d83121cd574c7e3
Matt Thalman <mthalman@microsoft.com>
2022-12-04 18:45:16 -0600

    Adds "image save-layers" command (#32)

---
+++ src/Valleysoft.Dredge/FileHelper.cs
@@ -0,0 +1,36 @@
+﻿namespace Valleysoft.Dredge;
+
+internal static class FileHelper
+{
+    public static void CopyDirectory(string sourceDir, string destinationDir)
+    {
+        DirectoryInfo dir = new(sourceDir);
+        if (!dir.Exists)
+        {
+            throw new DirectoryNotFoundException($"Source directory not found: {dir.FullName}");
+        }
+
+        Directory.CreateDirectory(destinationDir);
+
+        DirectoryInfo[] dirs = dir.GetDirectories();
+        foreach (FileInfo file in dir.GetFiles())
+        {
+            string targetFilePath = Path.Combine(destinationDir, file.Name);
+
+            if (file.LinkTarget is not null)
+            {
+                File.CreateSymbolicLink(targetFilePath, file.LinkTarget);
+            }
+            else
+            {
+                file.CopyTo(targetFilePath);
+            }
+        }
+        
+        foreach (DirectoryInfo subDir in dirs)
+        {
+            string newDestinationDir = Path.Combine(destinationDir, subDir.Name);
+            CopyDirectory(subDir.FullName, newDestinationDir);
+        }
+    }
+}
